d9c5aa27551ec54c02a98d18c5711ddba29698ea
Mehret Salif <41153874+DreaMBG@users.noreply.github.com>
2025-09-25 09:06:03 +0300

    Add files via upload

---
+++ OOP/operatori.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicheski_pobitovi_operatori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Aritmetichni operatori\n");
+            int a = 10; int b = 3;
+            Console.WriteLine($"(a + b) = {a + b}");
+            Console.WriteLine($"(a - b) = {a - b}");
+            Console.WriteLine($"(a * b) = {a * b}");
+            Console.WriteLine($"(a / b) = {a / b}");
+            Console.WriteLine($"(a % b) = {a % b}\n");
+
+            Console.WriteLine("Logicheski operatori");
+            bool x = true, y = false;
+            Console.WriteLine($"x && y = {x && y}");
+            Console.WriteLine($"x || y = {x || y}");
+            Console.WriteLine($"!x = {!x}");
+
+            Console.WriteLine("Pobitovi operatori");
+            int m = 7, n = 3;
+            Console.WriteLine($"m | n = {m | n}");
+            Console.WriteLine($"m & n = {m & n}");
+            Console.WriteLine($"m >> n = {m >> n}");
+            Console.WriteLine($"m << n = {m << n}");
+            Console.WriteLine($"m ^ n = {m ^ n}");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
